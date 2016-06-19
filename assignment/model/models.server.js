90341dd8e0ec3377b10ff4628aced57f8191050d
Zewei Chen <ScottChen@Scotts-MacBook-Pro.local>
2016-06-18 23:21:14 -0400

    added facebook login

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,18 +1,15 @@
 module.exports = function() {
-    
-    // var mongoose = require('mongoose');
-    // //mongoose.connect('mongodb://localhost/cs4550summer1');
-    // var connectionString = 'mongodb://127.0.0.1:27017/WebDev/assignment';
-    //
-    // if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    //     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-    //         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    //         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    //         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    //         process.env.OPENSHIFT_APP_NAME;
-    // }
-    //
-    // mongoose.connect(connectionString);
+
+    var mongoose = require('mongoose');
+    var connectionString = 'mongodb://127.0.0.1:27017/WebDev/assignment';
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+    mongoose.connect(connectionString);
     
     var userModel = require("./user/user.model.server.js")();
     var websiteModel = require("./website/website.model.server.js")();
--- server.js
+++ server.js
@@ -2,19 +2,19 @@ var express = require('express');
 var app = express();
 
 
-var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/cs4550summer1');
-var connectionString = 'mongodb://127.0.0.1:27017/webdev';
-
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
-
-mongoose.connect(connectionString);
+// var mongoose = require('mongoose');
+// //mongoose.connect('mongodb://localhost/cs4550summer1');
+// var connectionString = 'mongodb://127.0.0.1:27017/webdev';
+//
+// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+//         process.env.OPENSHIFT_APP_NAME;
+// }
+//
+// mongoose.connect(connectionString);
 
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
