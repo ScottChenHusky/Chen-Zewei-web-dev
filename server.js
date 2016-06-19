51983444f141ddda8fb81d6b80d7dd17490ae0f1
Zewei Chen <ScottChen@Scotts-MacBook-Pro.local>
2016-06-18 23:49:37 -0400

    added facebook login

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
-
-var connectionString = 'mongodb://127.0.0.1:27017/WebDev/assignment';
-
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
-
-var mongoose = require("mongoose");
-mongoose.connect(connectionString);
+//
+// var connectionString = 'mongodb://127.0.0.1:27017/WebDev/assignment';
+//
+// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+//         process.env.OPENSHIFT_APP_NAME;
+// }
+//
+// var mongoose = require("mongoose");
+// mongoose.connect(connectionString);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,7 +35,6 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 require('./assignment/app.js')(app);
-require('.test/app.js')(app);
 
 
 app.listen(port, ipaddress);
