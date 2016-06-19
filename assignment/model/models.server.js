f75e7ff5fa6fa5973369e0fd11c43ab4d0903599
Zewei Chen <ScottChen@Scotts-MacBook-Pro.local>
2016-06-18 23:30:58 -0400

    added facebook login

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,7 +1,7 @@
 module.exports = function() {
 
-    var mongoose = require('mongoose');
     var connectionString = 'mongodb://127.0.0.1:27017/WebDev/assignment';
+
     if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
             process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
@@ -9,6 +9,8 @@ module.exports = function() {
             process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
             process.env.OPENSHIFT_APP_NAME;
     }
+
+    var mongoose = require("mongoose");
     mongoose.connect(connectionString);
     
     var userModel = require("./user/user.model.server.js")();
