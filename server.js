c02797101aba8b5fb7763c369d2264b0aa220194
Zewei Chen <ScottChen@Scotts-MacBook-Pro.local>
2016-06-18 23:34:30 -0400

    added facebook login

--- server.js
+++ server.js
@@ -2,24 +2,23 @@ var express = require('express');
 var app = express();
 
 
-// var mongoose = require('mongoose');
-// //mongoose.connect('mongodb://localhost/cs4550summer1');
-// var connectionString = 'mongodb://127.0.0.1:27017/webdev';
-//
-// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-//         process.env.OPENSHIFT_APP_NAME;
-// }
-//
-// mongoose.connect(connectionString);
-
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
+
+var connectionString = 'mongodb://127.0.0.1:27017/WebDev/assignment';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var mongoose = require("mongoose");
+mongoose.connect(connectionString);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
