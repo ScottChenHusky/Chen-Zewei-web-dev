b632d2dd3f98176f487a5ce45fe6fc6729721769
Zhenhuan Wu <wu.zhenh@husky.neu.edu>
2016-06-24 01:39:23 -0400

    song schema/model/service

--- project/model/song/song.model.server.js
+++ project/model/song/song.model.server.js
@@ -1,5 +1,46 @@
-/**
- * Created by Zhenhuan on 6/24/2016.
- */
+module.exports = function() {
 
-2
+    var mongoose = require("mongoose");
+    var SongSchema = require("./song.schema.server")();
+    var Song = mongoose.model("Song", SongSchema);
+
+    var api = {
+        createSongForAlbum: createSongForAlbum,
+        findAllSongsForAlbum: findAllSongsForAlbum,
+        findSongById: findSongById,
+        updateSong: updateSong,
+        deleteSong: deleteSong
+    };
+    return api;
+
+
+    function createSongForAlbum(albumId, song) {
+        song._album = albumId;
+        return Song.create(song);
+    }
+
+    function findAllSongsForAlbum(albumId) {
+        return Song.find({_album: albumId});
+    }
+
+    function findSongById(songId) {
+        return Song.findById(songId);
+    }
+
+    function updateSong(songId, song) {
+        return Song.findOneAndUpdate({_id: songId}, song);
+        // return Song.update(
+        //     {_id: songId},
+        //     {$set :
+        //     {
+        //         name: website.name,
+        //         description: website.description
+        //     }
+        //     }
+        // );
+    }
+
+    function deleteSong(songId) {
+        return Song.remove({_id: songId});
+    }
+};
--- project/model/song/song.schema.server.js
+++ project/model/song/song.schema.server.js
@@ -1,4 +1,14 @@
-/**
- * Created by Zhenhuan on 6/24/2016.
- */
-1
+module.exports = function() {
+    var mongoose = require("mongoose");
+
+    var SongSchema = mongoose.Schema({
+        _album: {type: mongoose.Schema.Types.ObjectId, ref: 'Album'},
+        _musician: {type: mongoose.Schema.Types.ObjectId, ref: 'Musician'},
+        name: String,
+        url: String,
+        comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
+        dateCreated: {type: Date, default: Date.now}
+    }, {collection: "project.song"});
+
+    return SongSchema;
+};
