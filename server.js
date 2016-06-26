1afb74d6bf421f39d171c48aea2f244b7091f91d
Zhenhuan Wu <wu.zhenh@husky.neu.edu>
2016-06-26 19:00:55 -0400

    finish

--- server.js
+++ server.js
@@ -7,31 +7,31 @@ var passport = require('passport');
 
 
 var mongoose = require('mongoose');
-// var connectionString = 'mongodb://127.0.0.1:27017/webdev/assignment';
-//
-// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-//         process.env.OPENSHIFT_APP_NAME;
-// }
-//
-// mongoose.connect(connectionString);
+ var connectionString = 'mongodb://127.0.0.1:27017/webdev/assignment';
 
-mongoose.connect('mongodb://localhost/cs4550summer1');
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+mongoose.connect(connectionString);
+
+//mongoose.connect('mongodb://localhost/cs4550summer1');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(cookieParser());
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: true,
-//     saveUninitialized: true
-// }));
+ app.use(cookieParser());
+ app.use(session({
+     secret: process.env.SESSION_SECRET,
+     resave: true,
+     saveUninitialized: true
+ }));
 
-app.use(session({ secret: "thesecret" }));
+//app.use(session({ secret: "thesecret" }));
 
 app.use(passport.initialize());
 app.use(passport.session());
